3b8eecdc3b16ca2dcaf1fe3ce8cbbe1523934ed1
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2024-01-06 01:41:34 -0500

    general cube works

--- index.js
+++ index.js
@@ -1,1 +1,65 @@
-console.log(THREE);
+//Scene Objects Camera Renderer
+
+//Scene
+
+const scene = new THREE.Scene();
+
+//Mesh
+
+const geometry = new THREE.BoxGeometry(1, 1, 5);
+const material = new THREE.MeshBasicMaterial({ color: "#FF6F00" });
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+//Camera
+
+const aspect = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+const camera = new THREE.PerspectiveCamera(75, aspect.width / aspect.height); //default near and far is 1, 2000
+
+camera.position.x = 3;
+camera.position.y = 2;
+
+camera.position.z = 3;
+scene.add(camera);
+//Renderer
+const canvas = document.querySelector(".draw");
+console.log(canvas);
+
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(aspect.width, aspect.height);
+renderer.render(scene, camera);
+// //Mesh
+
+// const cube = new THREE.Mesh(
+//   new THREE.BoxGeometry(1, 1, 1),
+//   new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// );
+
+// //Add cube to scene
+// scene.add(cube);
+
+// //Camera
+
+// const sizes = {
+//   width: 800,
+//   height: 600,
+// };
+
+// const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+// camera.position.z = 3;
+
+// scene.add(camera);
+
+// //Renderer
+
+// const canvas = document.querySelector(".webgl");
+// const renderer = new THREE.WebGLRenderer({
+//   canvas: canvas,
+// });
+// renderer.setSize(sizes.width, sizes.height);
+
+// renderer.render(scene, camera);
